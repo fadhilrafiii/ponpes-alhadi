d1a3fef2dc4763476306e660c4ccf8fedc21988d
fadhilrafiii <fadhil.muhammad@dansmultipro.com>
2022-12-05 20:58:00 +0700

    Test WYSIWYG in preprod

--- pages/test.jsx
+++ pages/test.jsx
@@ -4,36 +4,41 @@ import { useState } from 'react';
 
 import { COLORS } from 'constants/colors';
 import { convertToRaw, EditorState } from 'draft-js';
+import draftToHtml from 'draftjs-to-html';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 import LoadingSpinner from 'components/base/LoadingSpinner';
 
 const Editor = dynamic(() => import('react-draft-wysiwyg').then((mod) => mod.Editor), {
   ssr: false,
-  // loading: () => (
-  //   <div>
-  //     <LoadingSpinner size={40} color={COLORS.PrimaryDark} />
-  //   </div>
-  // ),
+  loading: () => (
+    <div>
+      <LoadingSpinner size={40} color={COLORS.PrimaryDark} />
+    </div>
+  ),
 });
 
 const Test = () => {
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
 
-  console.log(convertToRaw(editorState.getCurrentContent()));
+  console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())));
   return (
     <div>
       <Head>
         <title>Test WYSIWYG</title>
       </Head>
-      <div>
-        <div
-          style={{
-            margin: '40px auto',
-            maxWidth: '600px',
-            width: '80%',
-          }}
-        >
+      <div
+        style={{
+          width: '600px',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          margin: '40px auto',
+        }}
+      >
+        <input style={{ width: '100%', marginBottom: 20 }} placeholder="Input Title" />
+        <div>
           <Editor
             toolbarClassName="toolbar-class"
             wrapperClassName="wrapper-class"
