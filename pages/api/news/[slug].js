c64794648963de8a3da0a0905c509a499d056913
fadhilrafiii <fadhil.muhammad@dansmultipro.com>
2022-12-11 23:09:41 +0700

    Add logging

--- pages/api/news/[slug].js
+++ pages/api/news/[slug].js
@@ -12,7 +12,7 @@ const handler = async (req, res) => {
 
   if (method !== 'GET') return response(res, { status: 405, message: 'Method harus "GET"!' });
   const news = await News.findOne({ title: query.slug });
-  console.log(query.slug, news, await News.find({}));
+  console.log('LENGKAP', query.slug, news, await News.find({}));
 
   return response(res, {
     status: 200,
